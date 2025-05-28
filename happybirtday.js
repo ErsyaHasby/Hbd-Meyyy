0f762055115e2833936cb68d1505f0e3095ca790
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-05-28 12:03:01 +0700

    Update happybirtday.js

--- happybirtday.js
+++ happybirtday.js
@@ -7,7 +7,7 @@ let w = (c.width = window.innerWidth),
     // change the text in here //
     strings: ["HAPPY", "BIRTHDAY!", "MEYSA ANIZAR"],
     charSize: 30,
-    charSpacing: 30,
+    charSpacing: 25,
     lineHeight: 35,
 
     cx: w / 2,
